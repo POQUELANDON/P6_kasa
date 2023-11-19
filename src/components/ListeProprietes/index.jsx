ab6e1b1754fe24e5c7580fc906b0b585832aa09e
Natacha POQUE-LANDON <natacha.poque@hotmail.fr>
2023-11-19 16:00:29 +0100

    Retrait doublon url ListeProprietes

--- src/components/ListeProprietes/index.jsx
+++ src/components/ListeProprietes/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { LogementContext } from '../LogementContext';
 
-const ListeProprietes = ({ url='/logements.json', children }) => {
+const ListeProprietes = ({ url, children }) => {
   const [logementsData, setLogementsData] = useState([]);
 
   useEffect(() => {
